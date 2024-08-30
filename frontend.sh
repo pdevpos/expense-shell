7c3aea3cb566a03455d2556271be90f40420b805
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-08-30 23:38:12 +0530

    install nginx

---
+++ frontend.sh
@@ -0,0 +1,22 @@
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+  echo "execute script in root privileges"
+  exit 1
+fi
+validate()
+{
+if [ $1 -ne 0 ]
+then
+  echo "$2 failed"
+else
+  echo "$2 success"
+  fi
+}
+
+dnf install nginx -y
+validate $? "Install nginx"
+systemctl enable nginx
+validate $? "Enable nginx"
+systemctl start nginx
+validate $? "Start nginx"
