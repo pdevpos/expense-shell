79b21c317d2fdced4d217e0a2b289b11a464a4f6
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-08-31 00:07:39 +0530

    create logs

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,13 @@
 userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
+logfolder=/var/log/shell-script
+script_name=$0
+logfile=$logfolder/$script_name-$DATE.log
+mkdir -p $logfolder
 if [ $userid -ne 0 ]
 then
   echo "execute script in root privileges"
@@ -15,20 +24,20 @@ else
 }
 
 dnf install nginx -y
-validate $? "Install nginx"
+validate $? "Install nginx" &>>$logfile
 systemctl enable nginx
-validate $? "Enable nginx"
+validate $? "Enable nginx" &>>$logfile
 systemctl start nginx
-validate $? "Start nginx"
+validate $? "Start nginx" &>>$logfile
 rm -rf /usr/share/nginx/html/*
-validate $? "remove default nginx content"
+validate $? "remove default nginx content" &>>$logfile
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
-validate $? "download frontend code"
+validate $? "download frontend code" &>>$logfile
 cd /usr/share/nginx/html
-validate $? "move to this directory"
+validate $? "move to this directory" &>>$logfile
 unzip /tmp/frontend.zip
-validate $? "unarchive frontend code"
+validate $? "unarchive frontend code" &>>$logfile
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
-validate $? "copy expense conf from server"
+validate $? "copy expense conf from server" &>>$logfile
 systemctl restart nginx
-validate $? "restart nginx server"
+validate $? "restart nginx server" &>>$logfile
