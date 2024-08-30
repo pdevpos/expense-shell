6f39399fed605d1ecd49f9c5c8fdfc8a023735e9
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-08-30 21:54:10 +0530

    1.disable nodejs 2.enable nodejs version 20 3. install nodejs 4.messages store in logs

--- backend.sh
+++ backend.sh
@@ -3,22 +3,27 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
+script_name=$0
+logfolder=/var/log/shell-script
+logfile=/var/log/shell-script/$script_name-$DATE.log
+mkdir -p $logfolder
 if [ $userid -ne 0 ]; then
-  echo -e "$Y script executes in root privileges $userid $N"
+  echo -e "$Y script executes in root privileges $userid $N" &>>$logfile
   exit 1
 fi
 validate()
 {
 if [ $1 -ne 0 ]
 then
-    echo -e "$2 $R failed $N"
+    echo -e "$2 $R failed $N" &>>$logfile
 else
-  echo -e "$2 $R success $N"
+  echo -e "$2 $G success $N" &>>$logfile
 fi
 }
 dnf module disable nodejs -y
-validate $? "nodejs module disabled"
+validate $? "nodejs module disabled" &>>logfile
 dnf module enable nodejs:20 -y
-validate $? "nodejs module enabled"
+validate $? "nodejs module enabled" &>>$logfile
 dnf install nodejs -y
-validate $? "install nodejs"
+validate $? "install nodejs" &>>$logfile
